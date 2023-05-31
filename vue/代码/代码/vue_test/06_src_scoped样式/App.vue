730560b81f5ca51b8e7bc9daf1a68b4d53783e8c
qcsy <yiwenyun@outlook.com>
2023-05-31 23:47:37 +0800

    inital commit

---
+++ vue/代码/代码/vue_test/06_src_scoped样式/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h1 class="title">你好啊</h1>
+		<School />
+		<Student />
+	</div>
+</template>
+
+<script>
+import Student from './components/Student'
+import School from './components/School'
+
+export default {
+	name: 'App',
+	components: { School, Student }
+}
+</script>
+
+<style scoped>  /* scoped: 作用域, 只在当前组件中生效，但是在app.vue里面一般不用scoped，因为app.vue的属性值一般是公用的 */
+	.title {
+		color: red;
+	}
+</style>
---
+++ vue/代码/代码/自己的测试代码/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<school></school>
+		<student></student>
+	</div>
+</template>
+
+<script>
+import Student from '../vue_basic/19_单文件组件/Student.vue';
+import School from '../vue_basic/19_单文件组件/School.vue'
+export default {
+	components: { student, School },
+
+}
+</script>
+
+<style></style>
