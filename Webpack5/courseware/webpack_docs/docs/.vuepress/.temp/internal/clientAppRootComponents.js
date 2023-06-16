5b85c70bf9309acaaef1c3963837ddb24ab7c018
Orion <yiwenyun@outlook.com>
2023-06-16 17:34:32 +0800

    learn webpack

---
+++ Webpack5/courseware/webpack_docs/docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from 'F:/Program/Front-end-learning/Study/Webpack5/courseware/webpack_docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]
